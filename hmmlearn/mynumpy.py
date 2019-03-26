50e89b6366eb4c484b39f9f1ec79b16e082b1017
maciosz <a.macioszek@students.mimuw.edu.pl>
2019-03-26 11:46:16 +0100

    I forgot to track mynumpy.py.

---
+++ hmmlearn/mynumpy.py
@@ -0,0 +1,28 @@
+# source of idea: https://stackoverflow.com/questions/5350342/can-i-set-float128-as-the-standard-float-array-in-numpy
+from numpy import *
+
+_empty = empty
+_ndarray = ndarray
+_zeros = zeros
+_ones = ones
+_full = full
+
+def empty(*args, **kwargs):
+    kwargs.update(dtype=float128)
+    return _empty(*args, **kwargs)
+
+def ndarray(*args, **kwargs):
+    kwargs.update(dtype=float128)
+    return _ndarray(*args, **kwargs)
+
+def ones(*args, **kwargs):
+    kwargs.update(dtype=float128)
+    return _ones(*args, **kwargs)
+
+def zeros(*args, **kwargs):
+    kwargs.update(dtype=float128)
+    return _zeros(*args, **kwargs)
+
+def full(*args, **kwargs):
+    kwargs.update(dtype=float128)
+    return _full(*args, **kwargs)
